960bdbfeca3929cf1ba5daad95c5d5c744ba9e05
jean <jean.ma.1986@gmail.com>
2019-03-18 15:09:34 +0800

    feat: 对AsyncStorage进行重写,支持更多特性,可监听值变化

---
+++ __tests__/AsyncStorage.mock.js
@@ -0,0 +1,20 @@
+jest.mock("react-native", () => {
+    let _value = {};
+    return {
+        AsyncStorage: {
+            setItem: jest.fn((key, value) => {
+                _value[key] = value;
+                return Promise.resolve();
+            }),
+            getItem: jest.fn((key) => {
+                return Promise.resolve(_value[key]);
+            }),
+            clear:jest.fn(),
+            multiSet:jest.fn(),
+            multiGet:jest.fn(),
+            removeItem:jest.fn(),
+            multiRemove:jest.fn(),
+            getAllKeys:jest.fn()
+        }
+    }
+})
